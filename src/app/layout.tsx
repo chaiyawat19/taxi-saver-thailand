8d98f5188d152fb04c2b03e1d0be37814812541f
Chaiyawat <chaiyawat.sonachai@gmail.com>
2026-06-15 22:25:18 +0700

    -utility ใช้งาน font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,72 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+const generalSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Extralight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-ExtralightItalic.otf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-LightItalic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Italic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-MediumItalic.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-SemiboldItalic.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/GeneralSans_Complete_Font/GeneralSans-BoldItalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-general-sans",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Taxi Saver Thailand",
@@ -12,8 +80,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={generalSans.variable}>
+      <body className={generalSans.className}>{children}</body>
     </html>
   );
 }
+
